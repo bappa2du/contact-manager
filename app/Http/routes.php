94f56efc9be00854fb5ef3e5b5699de57e700982
bappa2du <iftakharul.alam@live.com>
2016-07-24 15:05:20 +0600

    working with db

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\User;
+
 
 
 Route::group(['prefix'=>'user'],function(){
@@ -13,3 +15,15 @@ Route::group(['prefix'=>'super-admin'],function(){
 Route::controller('auth','AuthController');
 
 Route::get('/', 'Web\HomeController@index');
+
+Route::get('admin',function(){
+	// dd(database_path());
+	User::create([
+		'email'=>'demo@demo.com',
+		'name'=>'Admin',
+		'mobile'=>'8801670752214',
+		'password'=>bcrypt('demo'),
+		'status'=>1,
+		]);
+	return 'done';
+});
