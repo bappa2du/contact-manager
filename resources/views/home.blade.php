c55963f1d16cc5ed0b3d816e59bc8147fe27430f
bappa2du <iftakharul.alam@live.com>
2016-07-25 14:18:42 +0600

    title change

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <base href="/">
-        <title>Prologue by HTML5 UP</title>
+        <title>Contact Manager</title>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
@@ -88,27 +88,7 @@
 
                 
 
-                <!-- About contact Manager -->
-                    <section id="about" class="three">
-                        <div class="container">
-
-                            <header>
-                                <h2>About Contact Manager</h2>
-                            </header>
-
-                            <a href="#" class="image featured"><img src="assets/user/images/pic08.jpg" alt="" /></a>
-
-                            <p>Tincidunt eu elit diam magnis pretium accumsan etiam id urna. Ridiculus
-                            ultricies curae quis et rhoncus velit. Lobortis elementum aliquet nec vitae
-                            laoreet eget cubilia quam non etiam odio tincidunt montes. Elementum sem
-                            parturient nulla quam placerat viverra mauris non cum elit tempus ullamcorper
-                            dolor. Libero rutrum ut lacinia donec curae mus vel quisque sociis nec
-                            ornare iaculis.</p>
-
-                        </div>
-                    </section>
-
-                <!-- Contact -->
+                
                     
 
             </div>
